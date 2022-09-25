24f8f0f3f66aa6e5facc41eb3f36908a417a4f72
Rondinele Guimaraes <rondineleg@outlook.com>
2022-09-25 17:01:28 -0300

    Change to .net 6.0

--- FreeTime/Program.cs
+++ FreeTime/Program.cs
@@ -1,23 +1,32 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using FreeTime.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace FreeTime
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+var connection = configuration["ConexaoSqlite:SqliteConnectionString"];
+builder.Services.AddDbContext<PersonagenContext>(options =>
+    options.UseSqlite(connection)
+);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-
-    
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
